62d83e17608e579eaaf31b29f204e75f2978484f
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-05-27 09:09:59 -0400

    Checking in solution for swap numbers.

---
+++ 01 - Easy/C#/SwapNumbers/SwapNumbersSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.SwapNumbers
+{
+    class SwapNumbersSolution
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] != String.Empty)
+            {
+                IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+                foreach (var singleLine in linesInAFile)
+                {
+                    if (singleLine != String.Empty)
+                    {
+                        var alphanumericArray = singleLine.Split(' ');
+
+                        var alphanumericArraySwapped = alphanumericArray.Select(
+                            alphanumbericValue => alphanumbericValue.Last() + alphanumbericValue.Substring(1, alphanumbericValue.Length - 2) + alphanumbericValue.First()).ToList();
+
+                        Console.WriteLine(String.Join(" ", alphanumericArraySwapped));
+                    }
+                }
+            }
+        }
+
+        static IEnumerable<string> ReadFile(string filePath)
+        {
+            string[] fileLines = { };
+
+            try
+            {
+                fileLines = File.ReadAllLines(filePath);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+
+            return fileLines.ToList();
+        }
+    }
+}
