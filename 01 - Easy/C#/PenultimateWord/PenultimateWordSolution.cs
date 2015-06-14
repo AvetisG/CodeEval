35d29082b8ff07dc029a447db1cb3a0c8f479f29
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-14 14:57:35 -0400

    Checking in solution for penultimate word.

---
+++ 01 - Easy/C#/PenultimateWord/PenultimateWordSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.PenultimateWord
+{
+	class PenultimateWordSolution
+	{
+		private static void _Main(string[] args)
+		{
+			if (args[0] != String.Empty)
+			{
+				IEnumerable<string> linesOfFile = ReadFile(args[0]);
+
+				foreach (var singleLine in linesOfFile)
+				{
+					var wordsInALine = singleLine.Split(' ');
+
+					Console.WriteLine(wordsInALine[wordsInALine.Length-2]);
+				}
+			}
+		}
+
+		private static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
