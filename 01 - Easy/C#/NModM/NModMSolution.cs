0d4f95bff3afad62eb8fb30aee300dbaa8844000
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-14 19:55:17 -0400

    Checking in the solution for N mod M problem where we supposed to use impersonate module.

---
+++ 01 - Easy/C#/NModM/NModMSolution.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.NModM
+{
+    class NModMSolution
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] != String.Empty)
+            {
+                IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+                foreach (var singleLine in linesInAFile)
+                {
+                    if (singleLine != String.Empty)
+                    {
+                        Console.WriteLine(GetNModM(Convert.ToInt32(singleLine.Split(',')[0]), Convert.ToInt32(singleLine.Split(',')[1])));
+                    }
+                }
+            }
+        }
+
+        private static int GetNModM(int N, int M)
+        {
+            return N - (N / M) * M;
+        }
+
+        static IEnumerable<string> ReadFile(string filePath)
+        {
+            string[] fileLines = { };
+
+            try
+            {
+                fileLines = File.ReadAllLines(filePath);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+
+            return fileLines.ToList();
+        }
+    }
+}
--- 01 - Easy/C#/SelfDescribingNumbers/SelfDescribingNumbersSolution.cs
+++ 01 - Easy/C#/SelfDescribingNumbers/SelfDescribingNumbersSolution.cs
@@ -7,7 +7,7 @@ namespace CodeEvalSolutions.SelfDescribingNumbers
 {
 	internal class SelfDescribingNumbersSolution
 	{
-		private static void Main(string[] args)
+		private static void _Main(string[] args)
 		{
             if (args[0] != String.Empty)
 			{
