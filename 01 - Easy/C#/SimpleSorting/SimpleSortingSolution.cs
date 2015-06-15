de36127a581deb071fbf09296d6cf5f3746085a9
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-15 08:54:43 -0400

    Checking in solution for simple sorting problem.

---
+++ 01 - Easy/C#/SimpleSorting/SimpleSortingSolution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.SimpleSorting
+{
+	class SimpleSortingSolution
+	{
+		static void _Main(string[] args)
+		{
+			if (args[0] != String.Empty)
+			{
+				IEnumerable<string> linesOfFile = ReadFile(args[0]);
+
+				foreach (var singleLine in linesOfFile)
+				{
+					Console.WriteLine(String.Join(" ", GetArrayOfNumbers(singleLine.Split(' '))));
+				}
+			}
+		}
+
+		static IEnumerable<decimal> GetArrayOfNumbers(IEnumerable<string> argSingleLine)
+		{
+			List<decimal> arrayOfNumbers = argSingleLine.Select(line => Convert.ToDecimal(line)).ToList();
+
+			return arrayOfNumbers.OrderBy(argArg => argArg);
+		}
+
+		static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
