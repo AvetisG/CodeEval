8491b99cecb3638a4c08a5de744acc3df293641c
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-15 21:22:48 -0400

    Checking in Hex to Decimal Solution.

---
+++ 01 - Easy/C#/HexToDecimal/HexToDecimalSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.HexToDecimal
+{
+    class HexToDecimalSolution
+    {
+        private static void _Main(string[] args)
+        {
+            if (args[0] != String.Empty)
+            {
+                IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+                foreach (var singleLine in linesInAFile)
+                {
+                    Console.WriteLine(Convert.ToInt32(singleLine, 16));
+                }
+            }
+        }
+
+        static IEnumerable<string> ReadFile(string filePath)
+        {
+            string[] fileLines = { };
+
+            try
+            {
+                fileLines = File.ReadAllLines(filePath);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+
+            return fileLines.ToList();
+        }
+    }
+}
