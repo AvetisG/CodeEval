c14df3822d2b7189b421804194d6ab6383a87d81
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-17 09:07:33 -0400

    Checking in solution for File size.

---
+++ 01 - Easy/C#/FileSize/FileSizeSolution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace CodeEvalSolutions.FileSize
+{
+	class FileSizeSolution
+	{
+		private static void _Main(string[] args)
+		{
+			if (args[0] != String.Empty)
+			{
+				FileInfo fileInfo = new FileInfo(args[0]);
+				Console.WriteLine(fileInfo.Length);
+			}
+		}
+	}
+}
