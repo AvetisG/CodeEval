94afc77660b25ab3c3ed5574800bd740705f9c01
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-16 22:01:41 -0400

    Checking in the solution for unique element.

---
+++ 01 - Easy/C#/UniqueElement/UniqueElementSolution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.UniqueElement
+{
+	internal class UniqueElementSolution
+	{
+		static void _Main(string[] args)
+		{
+			if (args[0] != String.Empty)
+			{
+				IEnumerable<string> linesOfFile = ReadFile(args[0]);
+
+				foreach (var singleLine in linesOfFile)
+				{
+					var charArrayOfElements = singleLine.Split(',');
+					
+					Console.WriteLine(String.Join(",", charArrayOfElements.Distinct().ToList()));
+				}
+			}
+		}
+
+		static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
