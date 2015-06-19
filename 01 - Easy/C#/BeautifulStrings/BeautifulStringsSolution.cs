44e973f4a4fb953fe053c60e044f6188e9be6cfe
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-19 17:22:08 -0400

    Checking in the solution for roller coaster problem on CodeEval.

--- 01 - Easy/C#/BeautifulStrings/BeautifulStringsSolution.cs
+++ 01 - Easy/C#/BeautifulStrings/BeautifulStringsSolution.cs
@@ -7,7 +7,7 @@ namespace CodeEvalSolutions.BeautifulStrings
 {
 	class BeautifulStringsSolution
 	{
-		static void Main(string[] args)
+		static void _Main(string[] args)
 		{
 			if (args[0] != String.Empty)
 			{
---
+++ 01 - Easy/C#/RollerCoaster/RollerCoasterSolution.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CodeEvalSolutions.RollerCoaster
+{
+    class RollerCoasterSolution
+    {
+        static void Main(string[] args)
+        {
+            if (args[0] != String.Empty)
+            {
+                IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+                foreach (var singleLine in linesInAFile)
+                {
+                    if (singleLine != String.Empty)
+                    {
+						Console.WriteLine(GetRollerCoasterText(singleLine.ToCharArray()));
+                    }
+                }
+            }
+        }
+
+	    private static string GetRollerCoasterText(char[] argSingleLine)
+	    {
+		    bool upperCaseIt = true;
+		    var rollerCoasterText = new StringBuilder();
+
+			foreach (var letter in argSingleLine)
+		    {
+			    if (upperCaseIt && Char.IsLetter(letter))
+			    {
+				    rollerCoasterText.Append(letter.ToString().ToUpper());
+				    upperCaseIt = false;
+			    }
+				else if (!upperCaseIt && Char.IsLetter(letter))
+			    {
+				    rollerCoasterText.Append(letter.ToString().ToLower());
+				    upperCaseIt = true;
+			    }
+			    else
+			    {
+					rollerCoasterText.Append(letter);
+			    }
+		    }
+
+		    return rollerCoasterText.ToString();
+	    }
+
+	    static IEnumerable<string> ReadFile(string filePath)
+        {
+            string[] fileLines = { };
+
+            try
+            {
+                fileLines = File.ReadAllLines(filePath);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+
+            return fileLines.ToList();
+        }
+    }
+}
