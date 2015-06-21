e6fa96126dbbfd6be5cfaae4ce9a8178037daf6d
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-20 21:24:25 -0400

    Checking in draft for data recovery and this is definitely not the full solution I will be checking in the solution soon.

---
+++ 01 - Easy/C#/DataRecovery/DataRecoverySolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.DataRecovery
+{
+	class DataRecoverySolution
+	{
+		static void _Main(string[] args)
+		{
+			if (args[0] != String.Empty)
+			{
+				IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+				foreach (var singleLine in linesInAFile)
+				{
+					
+				}
+			}
+		}
+
+		static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
+
