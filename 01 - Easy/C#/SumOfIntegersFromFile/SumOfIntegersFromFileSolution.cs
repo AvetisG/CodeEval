15f0a86d0407007f5f8e2de5f54e3c76388fdaef
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-21 19:34:11 -0400

    Checking in solution for swap elements problem.

--- 01 - Easy/C#/SumOfIntegersFromFile/SumOfIntegersFromFileSolution.cs
+++ 01 - Easy/C#/SumOfIntegersFromFile/SumOfIntegersFromFileSolution.cs
@@ -7,7 +7,7 @@ namespace CodeEvalSolutions.SumOfIntegersFromFile
 {
 	class SumOfIntegersFromFileSolution
 	{
-		static void Main(string[] args)
+		static void _Main(string[] args)
 		{
 			if (args[0] != String.Empty)
 			{
---
+++ 01 - Easy/C#/SwapElements/SwapElementsSolution.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.SwapElements
+{
+    class SwapElementsSolution
+    {
+        private static void Main(string[] args)
+        {
+            if (args[0] != String.Empty)
+            {
+                IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+                foreach (var singleLine in linesInAFile)
+                {
+                    var stringToBeManipulated = singleLine.Split(':')[0];
+                    var howToManipulateString = singleLine.Split(':')[1];
+
+                    Console.WriteLine(SwapTheElements(
+                        stringToBeManipulated.Split(' ').Select(a => a).Where(a => !a.Equals(String.Empty)).ToList(), 
+                        howToManipulateString.Split(',').Select(a => a.Trim()))
+                        );
+                }
+            }
+        }
+
+        private static string SwapTheElements(IList<string> stringToBeManiulated, IEnumerable<string> swapParameters)
+        {
+            foreach (var swapParameter in swapParameters)
+            {
+                var firstSwap = Convert.ToInt32(swapParameter.Split('-')[0]);
+                var secondSwap = Convert.ToInt32(swapParameter.Split('-')[1]);
+
+                var temp = stringToBeManiulated[secondSwap];
+                stringToBeManiulated[secondSwap] = stringToBeManiulated[firstSwap];
+                stringToBeManiulated[firstSwap] = temp;
+            }
+
+            return String.Join(" ", stringToBeManiulated);
+        }
+
+        static IEnumerable<string> ReadFile(string filePath)
+        {
+            string[] fileLines = { };
+
+            try
+            {
+                fileLines = File.ReadAllLines(filePath);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+
+            return fileLines.ToList();
+        }
+    }
+}
