3145e03b401a6ce836b82920164363083a8fd409
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-21 15:20:29 -0400

    Checking in solution for Sum of integers problem.

--- 01 - Easy/C#/RacingChars/RacingCharsSolution.cs
+++ 01 - Easy/C#/RacingChars/RacingCharsSolution.cs
@@ -7,7 +7,7 @@ namespace CodeEvalSolutions.RacingChars
 {
     class RacingCharsSolution
     {
-        static void Main(string[] args)
+        static void _Main(string[] args)
         {
             if (args[0] != String.Empty)
             {
---
+++ 01 - Easy/C#/SumOfIntegersFromFile/SumOfIntegersFromFileSolution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.SumOfIntegersFromFile
+{
+	class SumOfIntegersFromFileSolution
+	{
+		static void Main(string[] args)
+		{
+			if (args[0] != String.Empty)
+			{
+				IEnumerable<string> listOfIntegers = ReadFile(args[0]);
+
+				int total = listOfIntegers.Aggregate(0, (current, singleInteger) => current + Convert.ToInt16(singleInteger));
+
+				Console.Write(total);
+			}
+		}
+
+		static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
+
