187261beb34928a55ac296725b55dfe80b9e7801
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-22 16:36:09 -0400

    Checking in solution for JSON menu ids.

--- 01 - Easy/C#/JsonMenuIds/JsonMenuIdsSolution.cs
+++ 01 - Easy/C#/JsonMenuIds/JsonMenuIdsSolution.cs
@@ -11,16 +11,48 @@ namespace CodeEvalSolutions.JsonMenuIds
 		{
 			if (args[0] != String.Empty)
 			{
-				IEnumerable<string> linesInAFile = ReadFile("file.txt");
+				IEnumerable<string> linesInAFile = ReadFile(args[0]);
 
 				foreach (var singleLine in linesInAFile)
 				{
 					if (singleLine != String.Empty)
 					{
-						Console.WriteLine();
+						var jsonData = singleLine.Replace("\"", "").Replace("\\", "").Split('[');
+						Console.WriteLine(GetLabeledSums(jsonData[1].Split(' ')));
 					}
 				}
 			}
+		}
+
+		private static int GetLabeledSums(string[] jsonDataCollection)
+		{
+			Stack<string> sumValidationStack = new Stack<string>();
+			var finalSumOfIds = 0;
+			var isLabeled = false;
+
+			for (int i=0; i<jsonDataCollection.Count(); i++)
+			{
+				if (jsonDataCollection[i].Contains("{"))
+				{
+					sumValidationStack.Push(jsonDataCollection[i + 1]);
+				}
+				else if (jsonDataCollection[i].Contains("label"))
+				{
+					isLabeled = true;
+				}
+				else if (jsonDataCollection[i].Contains("}"))
+				{
+					if (isLabeled)
+					{
+						var idValue = sumValidationStack.Peek();
+						finalSumOfIds = finalSumOfIds + Convert.ToInt32(idValue.Replace(",", ""));
+					}
+
+					isLabeled = false;
+				}
+			}
+
+			return Convert.ToInt32(finalSumOfIds);
 		}
 
 		static IEnumerable<string> ReadFile(string filePath)
