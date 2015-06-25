74b7ffc59d511b63c2e547223174d956a9803709
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-25 09:12:17 -0400

    Checking in solution for lowercase.

---
+++ 01 - Easy/C#/Lowercase/LowercaseSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.Lowercase
+{
+	class LowercaseSolution
+	{
+		static void Main(string[] args)
+		{
+			if (args[0] != String.Empty)
+			{
+				IEnumerable<string> linesOfLowercaseableText = ReadFile(args[0]);
+				
+				foreach (var lineOfLowercaseableText in linesOfLowercaseableText)
+				{
+					Console.WriteLine(lineOfLowercaseableText.ToLower());	
+				}
+			}
+		}
+
+		static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
+
--- 01 - Easy/C#/SumOfDigits/SumOfDigitsSolution.cs
+++ 01 - Easy/C#/SumOfDigits/SumOfDigitsSolution.cs
@@ -7,7 +7,7 @@ namespace CodeEvalSolutions.SumOfDigits
 {
 	class SumOfDigitsSolution
 	{
-		static void Main(string[] args)
+		static void _Main(string[] args)
 		{
 			if (args[0] != String.Empty)
 			{
