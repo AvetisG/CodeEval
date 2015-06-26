27836f32f86c12da0d16286f793ca79101109146
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-26 08:57:44 -0400

    Checking in a solution for the multiples of a number problem.

---
+++ 01 - Easy/C#/MultiplesOfANumber/MultiplesOfANumberSolution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.MultiplesOfANumber
+{
+    class MultiplesOfANumberSolution
+    {
+        private static void Main(string[] args)
+        {
+            if (args[0] != String.Empty)
+            {
+                IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+                foreach (var singleLine in linesInAFile)
+                {
+                    var limit = Convert.ToInt16(singleLine.Split(',')[0]);
+                    var n = Convert.ToInt16(singleLine.Split(',')[1]);
+
+                    Console.WriteLine(FindMultiple(limit, n));
+                }
+            }
+        }
+
+        private static int FindMultiple(int limit, int s)
+        {
+            int increment = s;
+
+            while (s < limit)
+            {
+                s = s + increment;
+            }
+            return s;
+        }
+
+        static IEnumerable<string> ReadFile(string filePath)
+        {
+            string[] fileLines = { };
+
+            try
+            {
+                fileLines = File.ReadAllLines(filePath);
+            }
+            catch (Exception e)
+            {
+                throw e.InnerException;
+            }
+
+            return fileLines.ToList();
+        }
+    }
+}
