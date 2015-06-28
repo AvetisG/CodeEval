e2ce9b6441f6c73a4f11f2a89eb302a44d174329
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-28 16:16:25 -0400

    Checking in the solution for the details problem.

---
+++ 01 - Easy/C#/Details/DetailsSolution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CodeEvalSolutions.Details
+{
+	class DetailsSolution
+	{
+		static void Main(string[] args)
+		{
+            if (args[0] != String.Empty)
+			{
+                IEnumerable<string> linesInAFile = ReadFile(args[0]);
+
+				foreach (var singleLine in linesInAFile) Console.WriteLine(CountDetailMoves(singleLine.Split(',')));
+			}
+		}
+
+	    private static int CountDetailMoves(IList<string> details)
+	    {
+	        List<int> moveCounts = new List<int>();
+
+	        for (int i=0; i<details.Count(); i++)
+	        {
+	            var currentValue = details[i];
+
+                if (currentValue == "XYYYY.Y") currentValue = "XYYYYYY"; // Apparently there is an error in the input that hasn't been fixed credit goes to StevenDunn
+
+                moveCounts.Add(currentValue.Count(a => a.Equals('.')));
+	        }
+
+	        return moveCounts.OrderBy(a => a).ToList()[0];
+	    }
+
+	    static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
+
