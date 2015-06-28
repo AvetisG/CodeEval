518fb6511badde2033a30ea400546105eef18cb6
Avetis Ghukasyan <avetis.ghukasyan@gmail.com>
2015-06-28 19:14:30 -0400

    Checking in solution for shortest repetition.

--- 01 - Easy/C#/Details/DetailsSolution.cs
+++ 01 - Easy/C#/Details/DetailsSolution.cs
@@ -7,7 +7,7 @@ namespace CodeEvalSolutions.Details
 {
 	class DetailsSolution
 	{
-		static void Main(string[] args)
+		static void _Main(string[] args)
 		{
             if (args[0] != String.Empty)
 			{
---
+++ 01 - Easy/C#/ShortestRepetition/ShortestRepetitionSolution.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CodeEvalSolutions.ShortestRepetition
+{
+    class ShortestRepetitionSolution
+	{
+		static void Main(string[] args)
+		{
+            if (args[0] != String.Empty)
+			{
+                IEnumerable<string> linesOfFile = ReadFile(args[0]);
+
+				foreach (var singleLine in linesOfFile)
+				{
+                    Console.WriteLine(FindShortestRepetition(singleLine));	
+				}
+			}
+		}
+
+        private static int FindShortestRepetition(string singleLine)
+        {
+            var repeatingString = FindRepeatingString(singleLine);
+            return singleLine.Length / singleLine.Replace(repeatingString, " ").Count();
+        }
+
+        private static string FindRepeatingString(string singleLine)
+        {
+            if (singleLine == new String(singleLine[0], singleLine.Length)) return singleLine[0].ToString();
+
+            var charArrayOfRepeatingCharacters = singleLine.ToCharArray();
+            var pattern = new StringBuilder();
+
+            foreach (var character in charArrayOfRepeatingCharacters)
+            {
+                pattern.Append(character);
+
+                if (singleLine.Replace(pattern.ToString(), "").Length == 0) return pattern.ToString();
+            }
+
+            return pattern.ToString();
+        }
+
+        static IEnumerable<string> ReadFile(string filePath)
+		{
+			string[] fileLines = { };
+
+			try
+			{
+				fileLines = File.ReadAllLines(filePath);
+			}
+			catch (Exception e)
+			{
+				throw e.InnerException;
+			}
+
+			return fileLines.ToList();
+		}
+	}
+}
